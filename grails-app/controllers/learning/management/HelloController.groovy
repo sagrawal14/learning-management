25e59d0db7bf11bdc74ab9f7ecae712fc54087c0
Shashank Agrawal <shagrawal99@gmail.com>
2016-06-11 11:47:38 +0530

    Added my first GSP

--- grails-app/controllers/learning/management/HelloController.groovy
+++ grails-app/controllers/learning/management/HelloController.groovy
@@ -9,16 +9,8 @@ class HelloController {
     def salman() {
         render("Hello salman khan")
     }
+
     def signup() {
-        render("""<H1>Sign-up-form</H1>
-                  <form method="post" action="/learning-management/form/saveUser">
-                        FirstName:<input type=text name="firstName" placeholder="Enter firstname"><br><br>
-                        LastName:<input type=text name="lastName" placeholder="Enter Lastname"><br><br>
-                        Email-ID:<input type=text name="email" placeholder="Enter Email"><br><br>
-                        Password:<input type="password" name="pwd" placeholder="Enter Password"><br><brs>
-                        <input type="submit" value="submit">
-                  </form>
-               """)
     }
 }
 
---
+++ grails-app/views/hello/signup.gsp
@@ -0,0 +1,12 @@
+<H1>Sign-up-form</H1>
+<form method="post" action="/learning-management/form/saveUser">
+    FirstName:<input type=text name="firstName" placeholder="Enter firstname"><br><br>
+
+    LastName:<input type=text name="lastName" placeholder="Enter Lastname"><br><br>
+
+    Email-ID:<input type=text name="email" placeholder="Enter Email"><br><br>
+
+    Password:<input type="password" name="pwd" placeholder="Enter Password"><br><brs>
+
+    <input type="submit" value="submit">
+</form>
